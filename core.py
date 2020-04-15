a05092f1dffb0d0d850881ecb67cd684aeed188e
Ilya Bratenkov <ilia.bratenkov@yandex.ru>
2020-04-15 20:01:13 +0300

    Some features added

--- core.py
+++ core.py
@@ -12,57 +12,64 @@ appid = "9253b6f1e7514bef9be055032b45b851"
 
 app = Flask(__name__)
 
+@app.errorhandler(404) 
+def not_found(e): 
+  return render_template('error.html', err_text="Incorrect call. Status equal 404")
+
 @app.route('/')
 def index_page():
     return render_template('index.html')
 
 @app.route('/myweather', methods=['GET', 'POST'])
 def get_weather():
-    if request.method == 'POST':
-        input_city = request.form['myname']
+    try:
+        if request.method == 'POST':
+            input_city = request.form['myname']
         
-        try:
-            city_db = pd.read_json(path_or_buf='city_list.json')
-            city_id = city_db[city_db['name'] == input_city].head(1)['id']
-        except:
-            return render_template('error.html', err_text="Incorrect city_list parsing")
-        
-        if city_id.empty == False:  
             try:
-                
-                url = f'api.openweathermap.org/data/2.5/weather?id={city_id}&appid={appid}'
-                request_result = requests.get(url).json()
+                city_db = pd.read_json(path_or_buf='city_list.json')
+                city_id = city_db[city_db['name'] == input_city].head(1)['id']
+            except:
+                return render_template('error.html', err_text="Incorrect city_list parsing")
         
-                current_temp = request_result['main']['temp']
-                current_pressure = request_result['main']['pressure']
-                current_humidity = request_result['main']['humidity']
-                current_wind = request_result['wind']['speed']
+            if city_id.empty == False: # & isinstance(city_id, pd.Series):  
+                try:
+                    city_id = city_id.values[0]
+                    url = f'https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={appid}'
+                    request_result = requests.get(url).json()
+        
+                    current_temp = request_result['main']['temp']
+                    current_pressure = request_result['main']['pressure']
+                    current_humidity = request_result['main']['humidity']
+                    current_wind = request_result['wind']['speed']
             
  
         
-                if re.fullmatch(r'\d+\.*\d*', str(current_temp)) == None:
-                    return render_template('error.html', err_text="Incorrect Data Format")
+                    if re.fullmatch(r'\d+\.*\d*', str(current_temp)) == None:
+                        return render_template('error.html', err_text="Incorrect Data Format")
                 
-                if re.fullmatch(r'\d+\.*\d*', str(current_pressure)) == None:
-                    return render_template('error.html', err_text="Incorrect Data Format")
+                    if re.fullmatch(r'\d+\.*\d*', str(current_pressure)) == None:
+                        return render_template('error.html', err_text="Incorrect Data Format")
                 
-                if re.fullmatch(r'\d+\.*\d*', str(current_humidity)) == None:
-                    return render_template('error.html', err_text="Incorrect Data Format")
+                    if re.fullmatch(r'\d+\.*\d*', str(current_humidity)) == None:
+                        return render_template('error.html', err_text="Incorrect Data Format")
                 
-                if re.fullmatch(r'\d+\.*\d*', str(current_wind)) == None:
-                    return render_template('error.html', err_text="Incorrect Data Format")
+                    if re.fullmatch(r'\d+\.*\d*', str(current_wind)) == None:
+                        return render_template('error.html', err_text="Incorrect Data Format")
                 
-                current_temp = current_temp - 273.15
+                    current_temp = current_temp - 273.15
                 
-                return render_template('weather.html', myname = input_city, current_temp = current_temp, current_pressure = current_pressure,  current_humidity = current_humidity, current_wind = current_wind  )
+                    return render_template('weather.html', myname = input_city, current_temp = current_temp, current_pressure = current_pressure,  current_humidity = current_humidity, current_wind = current_wind  )
         
-            except:
-                return render_template('error.html', err_text="Incorrect DB parsing")
+                except:
+                    return render_template('error.html', err_text="Incorrect DB parsing")
+            else:
+                    return render_template('error.html', err_text="City is Unknown. Try another city")
+        
         else:
-              return render_template('error.html', err_text="City is Unknown. Try another city")
-        
-    else:
-        return render_template('error.html', err_text="Incorrect call")
+            return render_template('error.html', err_text="Incorrect call")
+    except:
+        return render_template('error.html', err_text="Some bugs on server")
     
 
 if __name__ == '__main__':
